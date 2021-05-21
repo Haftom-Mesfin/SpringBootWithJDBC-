bfed6e6931705f71242e9fba89519b77adb8a3e3
Haftom-Mesfin <sadtime1906@gmail.com>
2021-05-21 18:19:12 -0500

    Updated EmployeeServiceImpl

--- src/main/java/com/gebreselassie/service/impl/EmployeeServiceImpl.java
+++ src/main/java/com/gebreselassie/service/impl/EmployeeServiceImpl.java
@@ -1,33 +1,43 @@
 package com.gebreselassie.service.impl;
 
+import com.gebreselassie.dao.EmployeeDAO;
 import com.gebreselassie.model.Employee;
 import com.gebreselassie.service.EmployeeService;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.List;
-
+@Setter
+@Getter
 public class EmployeeServiceImpl implements EmployeeService {
+
+    private EmployeeDAO employeeDAO;
+
     @Override
     public void addEmployee(Employee employee) {
-
+        employeeDAO.createEmployee(employee);
     }
 
     @Override
     public Employee fetchEmployeeById(int employeeId) {
-        return null;
+
+        return employeeDAO.getEmployeeById(employeeId);
     }
 
     @Override
     public void deleteEmployeeById(int employeeId) {
-
+        employeeDAO.deleteEmployeeById(employeeId);
     }
 
     @Override
     public void updateEmployeeEmailById(String newEmail, int employeeId) {
-
+        employeeDAO.updateEmployeeEmailById(newEmail, employeeId);
     }
 
     @Override
     public List<Employee> getAllEmployeesInfo() {
-        return null;
+
+        return employeeDAO.getAllEmployeesDetails();
     }
 }
