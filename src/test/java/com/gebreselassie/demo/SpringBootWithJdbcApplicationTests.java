a00206bad5fa0928bd8eda3b13a07d3cee1b8720
Haftom-Mesfin <sadtime1906@gmail.com>
2021-05-21 15:58:41 -0500

    first commit

---
+++ src/test/java/com/gebreselassie/demo/SpringBootWithJdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gebreselassie.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWithJdbcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
