cb5df6d8f29d0c8772ebbd3e32e03680058504b4
Haftom-Mesfin <sadtime1906@gmail.com>
2021-05-24 16:08:56 -0500

    Updated EmployeeDAOImpl

--- src/main/java/com/gebreselassie/dao/impl/EmployeeDAOImpl.java
+++ src/main/java/com/gebreselassie/dao/impl/EmployeeDAOImpl.java
@@ -2,12 +2,28 @@ package com.gebreselassie.dao.impl;
 
 import com.gebreselassie.dao.EmployeeDAO;
 import com.gebreselassie.model.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
-
+@Component
 public class EmployeeDAOImpl implements EmployeeDAO {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
     @Override
     public void createEmployee(Employee employee) {
+        String sql = "INSERT INTO employee_table(employee_name, salary, email, gender) VALUES(?, ?, ?, ?)";
+        int update = jdbcTemplate.update(sql,
+                employee.getEmployeeName(),
+                employee.getSalary(),
+                employee.getEmail(),
+                employee.getGender());
+        if(update > 0){
+            System.out.println("Employee created ...");
+        }
 
     }
 
